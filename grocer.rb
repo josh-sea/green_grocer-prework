72a0cd923ab5923ca2645db2e3f7116a00e76bbc
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 09:26:51 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -18,7 +18,6 @@ end
 
 def apply_coupons(cart_hash, coupon_array)
   new_hash = {}
-  
     coupon_array.each do |ckeys, cvalues|
       cart_hash.each do |item, item_info|
         if cvalues == item && coupon_array[i][:num] <= cart_hash[item][:count]
