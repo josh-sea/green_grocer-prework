67b564e211bd3ae99c663b23fbbd9dabeee4697f
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 09:27:53 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -21,7 +21,7 @@ def apply_coupons(cart_hash, coupon_array)
   coupon_array.each do |i|
     i.each do |ckeys, cvalues|
       cart_hash.each do |item, item_info|
-        if cvalues == item && coupon_array[i][:num] <= cart_hash[item][:count]
+        if cvalues == item && i[:num] <= cart_hash[item][:count]
  #         binding.pry
           discount = "#{item} W/COUPON"
 #         binding.pry 
