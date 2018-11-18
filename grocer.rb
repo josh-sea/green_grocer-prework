7a817fc2f87946c224b380059121284b868ab569
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 09:32:45 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -69,7 +69,6 @@ def checkout(cart, coupons)
 end
 
 def checkout(cart, coupons)
- def checkout( cart, coupons)
    # code here
   cart = consolidate_cart(cart)
   cart = apply_coupons(cart, coupons)
